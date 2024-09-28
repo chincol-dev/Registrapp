d2be8a508b74b4bc7fd265c8a0ee7eb5767968dd
JeanPiffaut <piffaut.jean@gmail.com>
2024-09-28 17:42:14 -0300

    create auth service json

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'path/to/your/json'; // Cambia por la ruta correcta o API
+  private usersUrl = 'assets/services/auth/users.json';  // Cambia según tu ruta de JSON
 
   constructor(private http: HttpClient) {}
 
-  // Método para autenticar el usuario basado en email y contraseña
+  // Autenticación simulada con un archivo JSON local
   authenticate(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { email, password });
+    return this.http.get(this.usersUrl).pipe(
+        map((users: any) => {
+          const user = users.find((u: any) => u.email === email && u.password === password);
+          if (user) {
+            return { success: true };
+          } else {
+            return { success: false };
+          }
+        })
+    );
   }
 }
